8abd49280fffea04ce8388e68c96a6033bd51c52
Reza Shuvo <shuvoreza256@gmail.com>
2024-09-17 14:27:36 +0600

    Delete see directory

--- see/rate_employees/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from rate_employees.models import Employee
-
-
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'empid', 'enothi_id', 'ename', 'eemail', 'edesignation', 'edept', 'esection','edivision','edirectorate')
-# Register your models here.
-admin.site.register(Employee, EmployeeAdmin)
--- see/rate_employees/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RateEmployeesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rate_employees'
